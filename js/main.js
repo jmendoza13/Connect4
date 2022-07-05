40460d2db6d86028187322c8ccfed378e56cdcbf
jmendoza13 <justinmendoza13@gmail.com>
2022-07-04 22:53:34 -0700

    added markers/fixed nested arrays

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const COLORS = {
     '1': 'blue',
     '-1': 'red',
-    '0': 'white'
+    '0': 'white' 
 };
 
 /*----- app's state (variables) -----*/
@@ -13,28 +13,30 @@ let turn; // 1 or -1; 0 for no movement
 /*----- event listeners -----*/
 /*----- functions -----*/
 init (); // utilizing init to initialize for a function
-
 function init() {
     // board represents the outer array
     board = [
-        [1, 0, 0, 0, 0, -1] // Each 0 represents a row; this array represents column 1 [index 0]
-        [0, 0, 0, 0, 0, 0] // column 2 [index 1]
-        [0, 0, 0, 0, 0, 0] // column 3 [index 2]
-        [0, 0, 0, 0, 0, 0] // column 4 [index 3]
-        [0, 0, 0, 0, 0, 0] // column 5 [index 4]
-        [0, 0, 0, 0, 0, 0] // column 6 [index 5]
-        [-1, 0, 0, 0, 0, 1] // column 7 [index 6]
+        [0, 0, 0, 0, 0, 0], // Each 0 represents a row; this array represents column 1 [index 0]
+        [0, 0, 0, 0, 0, 0], // column 2 [index 1]
+        [0, 0, 0, 0, 0, 0], // column 3 [index 2]
+        [0, 0, 0, 0, 0, 0], // column 4 [index 3]
+        [0, 0, 0, 0, 0, 0], // column 5 [index 4]
+        [0, 0, 0, 0, 0, 0], // column 6 [index 5]
+        [0, 0, 0, 0, 0, 0], // column 7 [index 6]
     ];
     turn = 1;
     render();
 }
 
 function render() {
-    // iterating over the column arrays (defined by 'board')
-    board.forEach(function(columnArr, columnIdx) {
-        columnArr.forEach(function(slotVal, rowIdx) {
-            const slotEl = document.getElementById(`c${columnIdx}r${rowIdx}`);
-            slotEl.style.backgroundColor = COLORS[slotVal];
+    //iterating over the column arrays (defined by 'board')
+    board.forEach(function(colArr, colIdx) {
+       colArr.forEach(function(cellVal, rowIdx) {
+        let cellEl = document.getElementById(`c${colIdx}r${rowIdx}`);
+        cellEl.style.backgroundColor = COLORS[cellVal];
         });
     });
 }
+
+
+
--- pseudocode/Connect4.md
+++ pseudocode/Connect4.md
@@ -5,18 +5,27 @@
 2. Create 2 players
     2a. define player 1 & 2
     2b. assign colors to player 1 & 2
-    2c. put a statement of "Player '_' Turn"
-    3d. alternate turns with each player
+    2c. put a statement of "Player '_' Turn" under the connect 4 header
+        2c. this should be a function that will alternate player turn and indicate it by a color
+    2e. alternate turns with each player/ no player can go 2 times in a row
 3. Create a Play again button
+    3a. seperate the play again button from the board so that it's nicely seprated/presented
+    3b. play again button shouldnt be allowed to be pressed until after the game is completed( i.e won/lost/tied)
+    3c. 
 4. set a text box that explains the rules on the side
     4a. create a button that opens the rules in a text box
-    4b. 
+    4b. set that text box on the right of the board
 5. Determine the GameStatus
     5a. Winner, Loser, Tie
     5b. game ends when a palyer connects 4 of their respective color in a row
-    5c.
+    5c. game is tied if neither player connects 4 pieces of their respective color in a row
 6. Setting certain functions
     6a. prevent palyers from putting pieces anywhere they want.
     6b. ^-this function should allow the player to only play in the first row and then move upwards one row at a time
-    6c.
+    6c. (potential idea) create a row at the top that will allow you to place a piece in a respective column
+    6d. assign a color value on the left of the board that will indicate what color belongs to each palyer
+    6e. (potential idea) allow players to select their own color of the board
+    6f. prevent players from placing a connect 4 piece that exists in a cell already.
+
+
 
